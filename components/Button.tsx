f8c180b412fce9aa15dcbc51ddaf79778e049139
mayados <mariannedos.cours@gmail.com>
2024-10-23 08:45:38 +0200

    add property specifyBackground and function to choose the background color of a button

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,17 +6,27 @@ import {LucideIcon} from 'lucide-react'
 interface ButtonProps {
   label:string;
   icon: LucideIcon;
-//   actionButton?: () => void;
-//   path: any
- 
+  specifyBackground: string;
+  action?: () => void; 
 }
 
-const Button:React.FC<ButtonProps> = ({label, icon: Icon}) => {
+const findBackgroundColor =  (specifyBackground)  => {
+  if(specifyBackground === "none"){
+    return "";
+  } else{
+    return "bg-pink-600"
+  }
+
+}
+
+const Button:React.FC<ButtonProps> = ({label, icon: Icon, specifyBackground, action}) => {
+
+  const backgroundColor = findBackgroundColor(specifyBackground);
+
   return (
-        <button className='flex f px-5 py-2 mt-1 bg-pink-600 from-white hover:bg-pink-500 cursor-pointer rounded-lg'>
+        <button onClick={action} className={`flex f px-5 py-2 mt-1 ${backgroundColor} from-white hover:bg-pink-500 cursor-pointer rounded-lg`}>
             <Icon className="mx-2" /> 
             {label} 
-
         </button>
     )    
 
