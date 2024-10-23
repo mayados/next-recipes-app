fd6c79ea9171bd3431dd5404cf15fdfa8b196991
mayados <mariannedos.cours@gmail.com>
2024-10-23 08:44:53 +0200

    add recipe tools and compositions

--- app/recipes/[recipeSlug]/page.tsx
+++ app/recipes/[recipeSlug]/page.tsx
@@ -2,20 +2,24 @@
 
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import { Clock9 } from 'lucide-react';
+import { Clock9, Key } from 'lucide-react';
 import { Gauge } from 'lucide-react';
 import CategoryTag from "@/components/CategoryTag";
 import Button from "@/components/Button";
 import { Download } from 'lucide-react';
 import { Heart } from 'lucide-react';
 import {CldImage} from "next-cloudinary";
+import Title from "@/components/Title";
+import { ListChecks } from 'lucide-react';
+import { CookingPot } from 'lucide-react';
 
 
 const Recipe = ({params}: {params: {recipeSlug: string}}) => {
 
 
     const [recipe, setRecipe] = useState<RecipeType | null>(null)
-
+    const [recipetools, setTools] = useState<RecipeToolType[]>([]);
+    const [compositions, setIngredients] = useState<CompositionType[]>([]);
 
     useEffect(() => {
         const fetchRecipe = async () => {
@@ -24,7 +28,8 @@ const Recipe = ({params}: {params: {recipeSlug: string}}) => {
             const data: RecipeType = await response.json()
             // On hydrate la recette avec les données récupérées
             setRecipe(data)
-            // Car on a un use state pour les commentaire, on gérera leur état à part
+            // setTools(data.recipetools);
+            setIngredients(data.compositions)
 
         }
         // On appelle la fonction
@@ -32,10 +37,42 @@ const Recipe = ({params}: {params: {recipeSlug: string}}) => {
         // Le useEffect va être redéclenché si l'articleId change
     }, [params.recipeSlug])
 
+    // We create a function to display the tools of the recipe on the event click on the right button
+    // Undisplay the ingredients
+    const displayTools = async (recipeId: string) => {
+        try {
+            const response = await fetch(`/api/recipes/${recipe?.id}/tools`, {
+                method: "GET",
+            });
+            if (response.ok) {
+                setTools(recipetools)
+                // Si l'appel à l'API fonctionne, on undisplay la composition (=les ingrédients) localement
+                setIngredients([]);
+            }
+        } catch (error) {
+            console.error("Erreur lors de la récupération des outils :", error);
+        }
+      }
+
+    const displayIngredients = async (recipeId: string) => {
+        try {
+            const response = await fetch(`/api/recipes/${recipe?.id}/ingredients`, {
+                method: "GET",
+            });
+            if (response.ok) {
+                // Si l'appel à l'API fonctionne, on undisplay la recipetools (=les outils) localement
+                setTools([]);
+            }
+        } catch (error) {
+            console.error("Erreur lors de la récupération des outils :", error);
+        }
+      }
+
   return (
 
-    <>
-        <section className="lg:w-screen lg:flex h-[50vh] bg-gray-800 mt-5 ml-5 mr-5">
+    
+    <div className="p-5">
+        <section className="lg:w-screen lg:flex h-[50vh] bg-gray-800 mt-5">
             {/* Basic informations */}
             <div className="lg:flex-1 flex flex-col items-center justify-center h-full">
                 <h1 className="text-center mb-3 text-4xl">{recipe?.title}</h1>
@@ -55,7 +92,7 @@ const Recipe = ({params}: {params: {recipeSlug: string}}) => {
                     <Button icon={Heart} label="Favorite" />
                 </div>
             </div>
-            <div className="border-2 border-green-600 lg:flex-1">
+            <div className="lg:flex-1">
                 <CldImage
                     alt=""
                     src="cld-sample-5"
@@ -69,25 +106,61 @@ const Recipe = ({params}: {params: {recipeSlug: string}}) => {
             </div>
         </section>
         {/* Main instructions about the recipe */}
-        <section className="mt-10 flex mr-5 ml-5">
+        <section className="mt-10 flex">
             {/* Instructions */}
             <div className="flex-1">
-                <h2>Instructions</h2>
+                <Title label="Instructions" icon={ListChecks} />
                 <p>{recipe?.instructions}</p>
             </div>
             {/* Ingredients and tools */}
             <div className="flex-1">
-                <h2>Ingredients and tools</h2>
+                <Title label="Ingredients and Tools" icon={CookingPot} />
                 {/* Tabs buttons to choose to display ingredients or tools */}
                 <div className="bg-gray-600 p-2 flex gap-3 rounded-md">
-                    <Button label="Ingredients" icon={Gauge} />
-                    <Button label="Tools" icon={Gauge} />
+                    <Button key={recipe?.id} label="Ingredients" icon={Gauge} specifyBackground="" />
+                    <Button key={recipe?.slug} label="Tools" icon={Gauge} specifyBackground="none" action={() => displayTools(`${recipe?.id}`)} />
                 </div>
-                {
-                //     recipe.composition.ingredient.label.map((ingredients) => (
+                <section className="flex flex-wrap gap-3 border-2 border-gray-600 rounded-md">
+                    {
+                        compositions.map((composition) => (
+                            <div key={composition.ingredient.id} className="mt-4 flex flex-col items-center text-center w-2/12">
+                                <CldImage
+                                    className="rounded-md"
+                                    alt=""
+                                    src="cld-sample-5"
+                                    width="100"
+                                    height="100"
+                                    crop={{
+                                        type: 'auto',
+                                        source: true
+                                    }}
+                                />                           
+                                <p>{composition.ingredient.label}</p>                            
+                            </div>
+    
+                    ))                      
+                    }                    
+                    {
+                        recipetools.map((tools) => (
+                            <div className="mt-4 flex flex-col items-center text-center w-2/12">
+                                <CldImage
+                                    className="rounded-md"
+                                    alt=""
+                                    src="cld-sample-5"
+                                    width="100"
+                                    height="100"
+                                    crop={{
+                                        type: 'auto',
+                                        source: true
+                                    }}
+                                />                           
+                                <p>{tools.tool.label}</p>                            
+                            </div>
+    
+                    ))                      
+                    }                    
+                </section>
 
-                // ))                      
-                }
  
             </div>
         </section>
@@ -95,7 +168,7 @@ const Recipe = ({params}: {params: {recipeSlug: string}}) => {
         <section>
 
         </section>
-    </>
+    </div>
 
   )
 }
