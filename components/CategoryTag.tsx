668c1a96afd40c0aa5a4a3af3f99a3326776733f
mayados <mariannedos.cours@gmail.com>
2024-10-23 08:45:59 +0200

    delete useless comments and correct code

--- components/CategoryTag.tsx
+++ components/CategoryTag.tsx
@@ -1,42 +1,32 @@
-import React from 'react'
+import React from 'react';
 
-
-interface CategoryTagProps{
-    categoryName: string;
+interface CategoryTagProps {
+  categoryName?: string;
 }
 
-  // We have to change the color of the card in function of the value
-  const getColorCategoryTagClass = (categoryName): string => {
-    // if((categoryName) =="Dessert"){
-    //   return "bg-red-800" 
-    // }else if((categoryName)=="Starter"){
-    //   return "bg-green-400"
-    // }else{
-    // return "bg-gray-800"
-    // }
-    switch (categoryName) {
-      case 'Dessert':
-        return "bg-red-800" ; // Rouge pour "urgent"
-      case 'Starter':
-        return "bg-green-400"; // Jaune pour "important"
-      case 'Main':
-        return "bg-gray-800"; // Vert pour "normal"
-      default:
-        return 'bg-blue-800 text-black'; // Gris par défaut pour les autres
-    }
+// Fonction pour obtenir la classe de couleur en fonction du nom de la catégorie
+const getColorCategoryTagClass = (categoryName?: string): string => {
+  switch (categoryName) {
+    case 'Dessert':
+      return 'bg-red-800'; 
+    case 'Starter':
+      return 'bg-green-400'; 
+    case 'Main':
+      return 'bg-gray-800'; 
+    default:
+      return 'bg-blue-800 text-black'; 
   }
+};
 
-const CategoryTag:React.FC<CategoryTagProps> = ({ categoryName }) => {
-
-
-  const color = getColorCategoryTagClass({categoryName});
+const CategoryTag: React.FC<CategoryTagProps> = ({ categoryName }) => {
+  // Appel de la fonction pour obtenir la couleur de la catégorie
+  const color = getColorCategoryTagClass(categoryName);
 
   return (
-
-    <div className={` text-center w-[15] ${color}`}>
-        <p>{categoryName}</p>
+    <div className={`text-center w-[15] ${color}`}>
+      <p>{categoryName}</p>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryTag
+export default CategoryTag;
