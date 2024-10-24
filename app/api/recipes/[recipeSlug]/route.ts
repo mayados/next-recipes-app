635051017e5fce3498a266e4a10f9245aff23d98
mayados <mariannedos.cours@gmail.com>
2024-10-24 10:52:52 +0200

    add const suggestions and display them

--- app/api/recipes/[recipeSlug]/route.ts
+++ app/api/recipes/[recipeSlug]/route.ts
@@ -31,13 +31,28 @@ export async function GET(req: NextRequest, {params}: {params: {recipeSlug: stri
                     orderBy: {
                         createdAt: 'desc'
                     }
-                }
+                }, 
             }
             
         })
 
+        const suggestions = await db.recipe.findMany({
+            where: {
+                id: {not: recipe?.id},
+                category: recipe?.category
+            },
+            orderBy: {
+            createdAt: 'desc'
+            },
+            take: 3,
+        })
+
         // On return l'article trouvé
-        return NextResponse.json(recipe)
+        return NextResponse.json({
+            success: true,
+            recipe: recipe,
+            suggestions: suggestions 
+        });
 
     } catch (error) {
         // Comme on exécute le console.log sur le serveur, on ne le retrouvera pas dans la console du navigateur, mais dans le terminall où on exécute le projet.
