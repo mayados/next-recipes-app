2d95dfb6a2d1c482e48e54cfbf664b2527267c6a
mayados <mariannedos.cours@gmail.com>
2024-10-25 11:44:00 +0200

    Replace add query to post a comment to a recipe in the folder commentsRecipe

---
+++ app/api/comments/commentsRecipe/route.ts
@@ -0,0 +1,51 @@
+import { db } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+// Asynchrone : waits for a promise
+export async function POST(req: NextRequest)
+{
+
+    // specific method to retrieve body (with NextRequest)
+    const data = await req.json();  
+    const { text, userId, recipeId, createdAt } = data;
+
+    try{
+
+        // Retrieve the comment to delete
+        const comment = await db.comment.create({
+            data: {
+                text: String(text),
+                userId: String(userId),
+                recipeId: String(recipeId),
+                createdAt: createdAt,
+            }
+        })
+
+        const user = await db.user.findUnique({ where: { id: userId } }); 
+        const pseudo = user ? user.pseudo : "Unknown User" ;
+
+
+        return NextResponse.json({
+            success: true,
+            comment: { 
+                id: comment.id,
+                text: comment.text,
+                createdAt: comment.createdAt,
+                userId: comment.userId,
+                user: {
+                    pseudo: pseudo,
+                    picture: user?.picture
+                },
+                recipeId: comment.recipeId,
+            },
+        });
+
+    } catch (error) {
+        // servor side : SSR
+        console.log("[DATA]", error)
+
+        return new NextResponse("Internal error, {status: 500}")
+    }
+
+
+}
