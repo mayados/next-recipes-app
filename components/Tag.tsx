c781d03f8624b055ac2363383f09d361ad577bb0
mayados <mariannedos.cours@gmail.com>
2024-10-25 11:49:13 +0200

    change color of Tag

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -7,7 +7,7 @@ interface TagProps {
 const Tag:React.FC<TagProps> = ({name}) => {
   return (
     <span 
-        className='px-3 py-2 text-xs rounded-full bg-neutral-500 group-hover:bg-blue-400'
+        className='px-3 py-2 text-xs rounded-full bg-pink-600 group-hover:bg-blue-400'
     >
         {name}
     </span>
