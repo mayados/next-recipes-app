8774c42b476792b98b4cfc3f271250c46df264bf
mayados <mariannedos.cours@gmail.com>
2024-10-28 12:04:38 +0100

    change css for categoryTag

--- components/CategoryTag.tsx
+++ components/CategoryTag.tsx
@@ -23,7 +23,7 @@ const CategoryTag: React.FC<CategoryTagProps> = ({ categoryName }) => {
   const color = getColorCategoryTagClass(categoryName);
 
   return (
-    <div className={`text-center w-[15] ${color}`}>
+    <div className={`py-1 px-2 inline-block ${color} rounded-md mb-3`}>
       <p>{categoryName}</p>
     </div>
   );
