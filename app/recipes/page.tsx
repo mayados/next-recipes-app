7c4293412f354c5b657245286baadc56eabf9471
mayados <mariannedos.cours@gmail.com>
2024-10-29 08:44:47 +0100

    use headless ui to display tabs with categories

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -3,10 +3,16 @@
 import RecipeCard from "@/components/RecipeCard";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
+
 
 export default function Recipes() {
 
   const [recipes, setRecipes] = useState<RecipeType[]>([])
+  const [starters, setStarters] = useState<RecipeType[]>([])
+  const [mains, setMains] = useState<RecipeType[]>([])
+  const [desserts, setDesserts] = useState<RecipeType[]>([])
+ 
 
   useEffect(() => {
     const fetchRecipes = async () => {
@@ -14,7 +20,10 @@ export default function Recipes() {
       // On type la constante data. De cette façon, si nous récupérons autre chose que la structure établie au départ, il y aura une erreur
       const data: RecipeType[] =  await response.json()
       // J'hydrate mon objet article avec les datas récupérés
-      setRecipes(data)
+      setRecipes(data['recipes'])
+      setStarters(data['starters'])
+      setMains(data['mains'])
+      setDesserts(data['desserts'])
     }
 
     fetchRecipes()
@@ -23,14 +32,42 @@ export default function Recipes() {
   return (
 
     <>
-        <h1 className="text-3xl text-white">Recipes</h1>
+        <h1 className="text-3xl text-white ml-3">Recipes</h1>
         
-        <div className="flex gap-5 justify-around px-3">
-        {
-            recipes.map((recipe) => (
-            <RecipeCard key={recipe.id} recipe={recipe} categoryName={recipe.category.title} />         
-            ))}          
-        </div>
+      <TabGroup className="ml-3 my-3">
+        <TabList className="ml-3 my-3 flex gap-3">
+          <Tab className="text-lg lg:text-base flex data-[selected]:bg-pink-600  data-[hover]:bg-pink-500 p-2 rounded-md">All</Tab>
+          <Tab className="text-lg lg:text-base flex data-[selected]:bg-pink-600  data-[hover]:bg-pink-500 p-2 rounded-md">Starter</Tab>
+          <Tab className="text-lg lg:text-base flex data-[selected]:bg-pink-600  data-[hover]:bg-pink-500 p-2 rounded-md">Main</Tab>
+          <Tab className="text-lg lg:text-base flex data-[selected]:bg-pink-600  data-[hover]:bg-pink-500 p-2 rounded-md">Dessert</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel className="flex gap-5 px-3">
+            {
+              recipes.map((recipe) => (
+              <RecipeCard key={recipe.id} recipe={recipe} categoryName={recipe.category.title} isHealthy={recipe.isHealthy} IsVegan={recipe.IsVegan} difficultyLevel={recipe.difficulty} />         
+            ))}   
+          </TabPanel>
+          <TabPanel className="flex gap-5 px-3">
+            {
+              starters.map((starter) => (
+              <RecipeCard key={starter.id} recipe={starter} categoryName={starter.category.title} isHealthy={starter.isHealthy} IsVegan={starter.IsVegan} difficultyLevel={starter.difficulty} />         
+            ))}   
+          </TabPanel>
+          <TabPanel className="flex gap-5 px-3">
+            {
+              mains.map((main) => (
+              <RecipeCard key={main.id} recipe={main} categoryName={main.category.title} isHealthy={main.isHealthy} IsVegan={main.IsVegan} difficultyLevel={main.difficulty} />         
+            ))}   
+          </TabPanel>
+          <TabPanel className="flex gap-5 px-3">
+            {
+              desserts.map((dessert) => (
+              <RecipeCard key={dessert.id} recipe={dessert} categoryName={dessert.category.title} isHealthy={dessert.isHealthy} IsVegan={dessert.IsVegan} difficultyLevel={dessert.difficulty} />         
+            ))}   
+          </TabPanel>
+        </TabPanels>
+      </TabGroup>
     </>
 
   )
