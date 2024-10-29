7e5d0855d0b7a4918c9487b4dbcce9d20a03b29e
mayados <mariannedos.cours@gmail.com>
2024-10-29 08:44:03 +0100

    add queries for starters, mains and desserts

--- app/api/recipes/route.ts
+++ app/api/recipes/route.ts
@@ -13,7 +13,65 @@ export async function GET() {
             }
         })
 
-        return NextResponse.json(recipes)
+
+        const starters = await db.recipe.findMany({
+            where: {
+                category: {
+                    title: {
+                        contains: "starter",
+                        mode: 'insensitive',
+                    }
+                }
+            },
+            orderBy: {
+                createdAt: 'desc'
+            },
+            include: {
+                category: true
+                }
+        })
+
+        const mains = await db.recipe.findMany({
+            where: {
+                category: {
+                    title: {
+                        contains: "main",
+                        mode: 'insensitive',
+                    }
+                }
+            },
+            orderBy: {
+                createdAt: 'desc'
+            },
+            include: {
+                category: true
+                }
+        })
+
+        const desserts = await db.recipe.findMany({
+            where: {
+                category: {
+                    title: {
+                        contains: "dessert",
+                        mode: 'insensitive',
+                    }
+                }
+            },
+            orderBy: {
+                createdAt: 'desc'
+            },
+            include: {
+                category: true
+                }
+        })
+
+        return NextResponse.json({
+            success: true,
+            recipes: recipes,
+            starters: starters,
+            mains: mains,
+            desserts: desserts,
+        });
 
     } catch (error) {
 
