060d1dc1aa6e05e6848afceb006897878c88659b
mayados <mariannedos.cours@gmail.com>
2024-10-30 08:36:35 +0100

    display another vue if the user is signed in or not

--- components/CommentForm.tsx
+++ components/CommentForm.tsx
@@ -4,6 +4,8 @@ import {LucideIcon} from 'lucide-react'
 import { Field, Textarea } from '@headlessui/react';
 import Button from './Button';
 import { SendHorizontal } from 'lucide-react';
+import { useAuth } from '@clerk/nextjs';
+
 
 
 
@@ -19,14 +21,24 @@ interface CommentFormProps {
 
 const CommentForm:React.FC<CommentFormProps> = ({name, placeholder, action, type, onChange, value}) => {
 
+  // Verify if the user is connected
+  const { isSignedIn } = useAuth();
+
   return (
+    <>
+    {isSignedIn ? (
         <form onSubmit={action} className="flex flex-col justify-center items-center">
             <Field className="w-full">
                 <Textarea name={name} onChange={onChange} placeholder={placeholder} value={value} className="w-full min-h-[10vh] rounded-md bg-gray-700 text-white p-3"></Textarea>
             </Field>
             <Button icon={SendHorizontal} label="Send" specifyBackground="" type={type} />
-        </form>
-    )    
+        </form>      
+    ) : (
+      <p>Veuillez vous connecter pour soumettre un commentaire</p>    
+  
+    )}
+  </>
+  )    
 
 }
 
