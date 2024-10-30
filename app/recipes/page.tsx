857e7d666bb60709fea3940571d1c5c221ed90e2
mayados <mariannedos.cours@gmail.com>
2024-10-30 08:36:13 +0100

    specify flex col and flex row for md

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -42,25 +42,25 @@ export default function Recipes() {
           <Tab className="text-lg lg:text-base flex data-[selected]:bg-pink-600  data-[hover]:bg-pink-500 p-2 rounded-md">Dessert</Tab>
         </TabList>
         <TabPanels>
-          <TabPanel className="flex gap-5 px-3">
+          <TabPanel className="flex-row md:flex-row gap-5 px-3">
             {
               recipes.map((recipe) => (
               <RecipeCard key={recipe.id} recipe={recipe} categoryName={recipe.category.title} isHealthy={recipe.isHealthy} IsVegan={recipe.IsVegan} difficultyLevel={recipe.difficulty} />         
             ))}   
           </TabPanel>
-          <TabPanel className="flex gap-5 px-3">
+          <TabPanel className="flex-row md:flex gap-5 px-3">
             {
               starters.map((starter) => (
               <RecipeCard key={starter.id} recipe={starter} categoryName={starter.category.title} isHealthy={starter.isHealthy} IsVegan={starter.IsVegan} difficultyLevel={starter.difficulty} />         
             ))}   
           </TabPanel>
-          <TabPanel className="flex gap-5 px-3">
+          <TabPanel className="flex-row md:flex gap-5 px-3">
             {
               mains.map((main) => (
               <RecipeCard key={main.id} recipe={main} categoryName={main.category.title} isHealthy={main.isHealthy} IsVegan={main.IsVegan} difficultyLevel={main.difficulty} />         
             ))}   
           </TabPanel>
-          <TabPanel className="flex gap-5 px-3">
+          <TabPanel className="flex-row md:flex gap-5 px-3">
             {
               desserts.map((dessert) => (
               <RecipeCard key={dessert.id} recipe={dessert} categoryName={dessert.category.title} isHealthy={dessert.isHealthy} IsVegan={dessert.IsVegan} difficultyLevel={dessert.difficulty} />         
