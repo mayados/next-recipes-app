187e6c05dc5ab94b7ad89273493f8eb2e88daba3
mayados <mariannedos.cours@gmail.com>
2024-11-07 10:43:54 +0100

    add query to find dbUser

--- app/api/comments/commentsArticle/route.ts
+++ app/api/comments/commentsArticle/route.ts
@@ -7,23 +7,37 @@ export async function POST(req: NextRequest)
 
     // specific method to retrieve body (with NextRequest)
     const data = await req.json();  
-    const { text, userId, articleId, createdAt } = data;
+    const { text, clerkUserId, createdAt, email, userPseudo, articleId } = data;
 
     try{
+        // We search if the user already exists in the database (beacause we use Clerk to manage users, but we have a local table User in the database)
+        let dbUser = await db.user.findUnique({
+            where: { 
+                clerkUserId: clerkUserId,
+            },
+            });
+              
+        // If the user doesn't exist in the database, we create him (because the authentication is made with clerk ,and we have to be sure to link a user of the database to the current user)
+        if (!dbUser) {
+            dbUser = await db.user.create({
+                data: {
+                    clerkUserId: clerkUserId,
+                    mail: email,
+                    pseudo: userPseudo,
+                    picture: "",
+                },
+            });
+        }
 
-        // Retrieve the comment to delete
+        // Creation of the comment
         const comment = await db.comment.create({
             data: {
                 text: String(text),
-                userId: String(userId),
+                userId: dbUser.id,
                 articleId: String(articleId),
                 createdAt: createdAt,
             }
         })
-
-        const user = await db.user.findUnique({ where: { id: userId } }); 
-        const pseudo = user ? user.pseudo : "Unknown User" ;
-
 
         return NextResponse.json({
             success: true,
@@ -33,8 +47,8 @@ export async function POST(req: NextRequest)
                 createdAt: comment.createdAt,
                 userId: comment.userId,
                 user: {
-                    pseudo: pseudo,
-                    picture: user?.picture
+                    pseudo: dbUser.pseudo,
+                    picture: dbUser?.picture
                 },
                 articleId: comment.articleId,
             },
@@ -43,6 +57,7 @@ export async function POST(req: NextRequest)
     } catch (error) {
         // servor side : SSR
         console.log("[DATA]", error)
+        console.log(comment)
 
         return new NextResponse("Internal error, {status: 500}")
     }
--- app/api/comments/commentsRecipe/route.ts
+++ app/api/comments/commentsRecipe/route.ts
@@ -7,23 +7,38 @@ export async function POST(req: NextRequest)
 
     // specific method to retrieve body (with NextRequest)
     const data = await req.json();  
-    const { text, userId, recipeId, createdAt } = data;
+    const { text, clerkUserId, recipeId, createdAt, email, userPseudo } = data;
 
     try{
 
-        // Retrieve the comment to delete
+        console.log("Les données reçues en back sont : "+data)
+        // We search if the user already exists in the database (beacause we use Clerk to manage users, but we have a local table User in the database)
+        let dbUser = await db.user.findUnique({
+            where: { 
+                clerkUserId: clerkUserId,
+            },
+        });
+                      
+        // If the user doesn't exist in the database, we create him (because the authentication is made with clerk ,and we have to be sure to link a user of the database to the current user)
+        if (!dbUser) {
+            dbUser = await db.user.create({
+                data: {
+                    clerkUserId: clerkUserId,
+                    mail: email,
+                    pseudo: userPseudo,
+                    picture: "",
+                },
+            });
+        }
+
         const comment = await db.comment.create({
             data: {
                 text: String(text),
-                userId: String(userId),
+                userId: dbUser.id,
                 recipeId: String(recipeId),
                 createdAt: createdAt,
             }
         })
-
-        const user = await db.user.findUnique({ where: { id: userId } }); 
-        const pseudo = user ? user.pseudo : "Unknown User" ;
-
 
         return NextResponse.json({
             success: true,
@@ -33,8 +48,8 @@ export async function POST(req: NextRequest)
                 createdAt: comment.createdAt,
                 userId: comment.userId,
                 user: {
-                    pseudo: pseudo,
-                    picture: user?.picture
+                    pseudo: dbUser.pseudo,
+                    picture: dbUser?.picture
                 },
                 recipeId: comment.recipeId,
             },
