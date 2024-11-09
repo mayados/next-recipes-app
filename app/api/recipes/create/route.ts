d5b89cb0ea10ff666d06badaec3cd694d0bd142a
mayados <mariannedos.cours@gmail.com>
2024-11-10 00:57:04 +0100

    reorganize code to create thing in the good order

--- app/api/recipes/create/route.ts
+++ app/api/recipes/create/route.ts
@@ -2,127 +2,106 @@ import { db } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
 // Asynchrone : waits for a promise
-export async function POST(req: NextRequest)
-{
-
-    // specific method to retrieve body (with NextRequest)
-    const data = await req.json();  
+export async function POST(req: NextRequest) {
+    const data = await req.json();
     const { title, instructions, preparationTime, isHealthy, isVegan, picture, createdAt, difficulty, slug, categoryTitle, ingredients,  clerkUserId, email, pseudo, steps, tools } = data;
-    try{
-        // console.log("données reçues api : "+data);
 
-        // We search if the user already exists in the database (beacause we use Clerk to manage users, but we have a local table User in the database)
+    try {
         let dbUser = await db.user.findUnique({
-            where: { 
-                clerkUserId: clerkUserId,
-            },
-          });
-      
-        //   If the user doesn't exist in the database, we create him (because the authentication is made with clerk ,and we have to be sure to link a user of the database to the current user)
-          if (!dbUser) {
+            where: { clerkUserId: clerkUserId },
+        });
+
+        if (!dbUser) {
             dbUser = await db.user.create({
-              data: {
-                clerkUserId: clerkUserId,
-                mail: email,
-                pseudo: pseudo,
-                picture: "",
-              },
+                data: {
+                    clerkUserId: clerkUserId,
+                    mail: email,
+                    pseudo: pseudo,
+                    picture: "",
+                },
             });
-          }
+        }
+        const category = await db.category.findUnique({
+            where: { title: categoryTitle },
+        });
+        const categoryId = category?.id;
 
-          const category = await db.category.findUnique({
-            where: { 
-                title: categoryTitle,
-            },
-          });
-
-          const categoryId = category?.id
-
-        // Retrieve the comment to delete
         const recipe = await db.recipe.create({
             data: {
                 title: String(title),
-                // the user we found or the user we just created
                 userId: dbUser.id,
                 instructions: String(instructions),
                 createdAt: createdAt,
                 timePreparation: parseInt(preparationTime),
-                isHealthy: Boolean(isVegan),
-                IsVegan: Boolean(isHealthy),
+                isHealthy: Boolean(isHealthy ?? false),
+                IsVegan: Boolean(isVegan ?? false),
                 picture: String(picture),
                 difficulty: parseInt(difficulty),
                 slug: String(slug),
                 categoryId: categoryId,
-            }
-        })
+            },
+        });
+
+        const ingredientEntries = await Promise.all(
+            ingredients.map(async (ingredient) => {
+                let existingIngredient = await db.ingredient.findUnique({
+                    where: { label: ingredient.name }
+                });
+
+                // Create ingredient if it doesn't exist
+                if (!existingIngredient) {
+                    existingIngredient = await db.ingredient.create({
+                        data: {
+                            label: String(ingredient.name),
+                            slug: ingredient.slug,  // Enregistre le slug sans vérification d'unicité
+                            picture: ingredient.picture || "pictureIngredient",
+                        },
+                    });
+                }
+                
+                return {
+                    ingredientId: existingIngredient.id,
+                    unit: String(ingredient.unity),
+                    quantity: parseInt(ingredient.quantity),
+                };
+            })
+        );
+
+        // create compositions after creating ingredients
+        const compositionPromises = ingredientEntries.map(async (entry) => {
+            await db.composition.create({
+                data: {
+                    unit: entry.unit,
+                    quantity: entry.quantity,
+                    recipeId: recipe.id,
+                    ingredientId: entry.ingredientId,
+                },
+            });
+        });
 
         const recipeSteps = steps.map(async (step) => {
-
             await db.step.create({
                 data: {
                     text: String(step.text),
                     number: parseInt(step.number),
                     recipeId: recipe.id,
-                }
+                },
             });
-
-        
-        await Promise.all(recipeSteps);
-
-
         });
-
-        // create query to post ingredients
-        // Each function is async, so we have to put await
-        const ingredientPromises = ingredients.map(async (ingredient) => {
-            // We have to verify if the ingredient exists. We don't want to stock it multiple times in the database
-            let existingIngredient = await db.ingredient.findUnique({
-                where: { 
-                    label: ingredient.name
-                 }
-            });
-
-            if (!existingIngredient) {
-                existingIngredient = await db.ingredient.create({
-                    data: {
-                        label: String(ingredient.name),
-                        slug: ingredient.slug,
-                        picture: ingredient.picture || "pictureIngredient",
-                    }
-                });
-            }
-
-            // Create query to post composition of the recipe
-            await db.composition.create({
-                data: {
-                    unit: String(ingredient.unity),
-                    quantity: parseInt(ingredient.quantity),
-                    recipeId: recipe.id,
-                    ingredientId: existingIngredient.id, 
-                }
-            });
-
-        // create query to post Steps
-
-
-        });
-
-        await Promise.all(ingredientPromises);
 
         const toolPromises = tools.map(async (tool) => {
             let existingTool = await db.tool.findUnique({
-                where: { 
-                    label: tool.label
-                 }
+                where: { label: tool.label }
             });
 
+            // if the tool doesn't exist, we create it
             if (!existingTool) {
                 existingTool = await db.tool.create({
                     data: {
                         label: String(tool.label),
                         slug: tool.slug,
                         picture: tool.picture || "pictureIngredient",
-                    }
+                    },
                 });
             }
 
@@ -130,30 +109,24 @@ export async function POST(req: NextRequest)
                 data: {
                     recipeId: recipe.id,
                     toolId: existingTool.id, 
-                }
+                },
             });
-
-
-
         });
 
-        await Promise.all(toolPromises);
+        // Wait for all the promises to be resolved
+        await Promise.all([...compositionPromises, ...recipeSteps, ...toolPromises]);
 
-        
         return NextResponse.json({
             success: true,
+            // We return the slug because we need to redirect to the new recipe's page
             recipeSlug: recipe.slug,
         });
 
     } catch (error) {
-        // servor side : SSR
-        console.log("[DATA]", error)
-
+        console.log("[DATA]", error);
         return NextResponse.json({
             success: false,
             error: error
         }, { status: 500 });
     }
-
-
 }
