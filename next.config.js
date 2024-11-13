ae34f7750f593f72ad39e8d328657b75bfd6c48c
mayados <mariannedos.cours@gmail.com>
2024-11-13 23:54:02 +0100

    correct hostname for clerk img

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
             },
             {
                 protocol: 'https',
-                hostname: 'images.clerk.com',
+                hostname: 'img.clerk.com',
             },
             {
                 protocol: 'https',
