252535340f15d4c57f0b2d05890f3f71b2e74a70
mayados <mariannedos.cours@gmail.com>
2024-11-13 23:58:50 +0100

    create component AdminNav

---
+++ components/AdminNav.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Link from "next/link";
+
+const AdminNav:React.FC = () => {
+  return (
+    <aside className="flex-4 h-screen rounded-md bg-slate-600">
+        <ul className="flex flex-col gap-5">
+        <li className="hover:bg-slate-800 p-2 hover:cursor-pointer"><Link href={`/admin/users`}>Users</Link></li>
+        <li className="hover:bg-slate-800 p-2 hover:cursor-pointer"><Link href={`/admin/recipes`}>Recipes</Link></li>
+        <li className="hover:bg-slate-800 p-2 hover:cursor-pointer"><Link href={`/admin/articles`}>Articles</Link></li>
+        <li className="hover:bg-slate-800 p-2 hover:cursor-pointer"><Link href={`/admin/comments-recipes`}>Comments recipes</Link></li>
+        <li className="hover:bg-slate-800 p-2 hover:cursor-pointer"><Link href={`/admin/comments-articles`}>Comments articles</Link></li>
+        </ul>
+    </aside>
+  )
+}
+
+export default AdminNav
