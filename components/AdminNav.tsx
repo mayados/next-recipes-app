3059a62a35263f5c59a36c61612deee50c3f0a26
mayados <mariannedos.cours@gmail.com>
2024-11-22 12:11:11 +0100

    replace li lines by Link

--- components/AdminNav.tsx
+++ components/AdminNav.tsx
@@ -3,17 +3,16 @@ import Link from "next/link";
 
 const AdminNav:React.FC = () => {
   return (
-    <aside className="flex-4 h-screen rounded-md bg-slate-600">
+  <aside className="w-[20vw] h-screen rounded-md bg-slate-600 p-2">
         <ul className="flex flex-col gap-5">
-        <li className="hover:bg-slate-800 p-2 hover:cursor-pointer"><Link href={`/admin/users`}>Users</Link></li>
-        <li className="hover:bg-slate-800 p-2 hover:cursor-pointer"><Link href={`/admin/recipes`}>Recipes</Link></li>
-        <li className="hover:bg-slate-800 p-2 hover:cursor-pointer"><Link href={`/admin/articles`}>Articles</Link></li>
-        <li className="hover:bg-slate-800 p-2 hover:cursor-pointer"><Link href={`/admin/comments-recipes`}>Comments recipes</Link></li>
-        <li className="hover:bg-slate-800 p-2 hover:cursor-pointer"><Link href={`/admin/comments-articles`}>Comments articles</Link></li>
-        <li className="hover:bg-slate-800 p-2 hover:cursor-pointer"><Link href={`/admin/tags`}>Tags</Link></li>
-        <li className="hover:bg-slate-800 p-2 hover:cursor-pointer"><Link href={`/admin/ingredients`}>Ingredients</Link></li>
-        <li className="hover:bg-slate-800 p-2 hover:cursor-pointer"><Link href={`/admin/tools`}>Tools</Link></li>
-        <li className="hover:bg-slate-800 p-2 hover:cursor-pointer"><Link href={`/admin/categories`}>Categories</Link></li>
+          <Link className='hover:bg-slate-800 p-2 hover:cursor-pointer' href={`/admin/recipes`}>Recipes</Link>
+          <Link className='hover:bg-slate-800 p-2 hover:cursor-pointer' href={`/admin/articles`}>Articles</Link>
+          <Link className='hover:bg-slate-800 p-2 hover:cursor-pointer' href={`/admin/comments-recipes`}>Comments recipes</Link>
+          <Link className='hover:bg-slate-800 p-2 hover:cursor-pointer' href={`/admin/comments-articles`}>Comments articles</Link>
+          <Link className='hover:bg-slate-800 p-2 hover:cursor-pointer' href={`/admin/tags`}>Tags</Link>
+          <Link className='hover:bg-slate-800 p-2 hover:cursor-pointer' href={`/admin/ingredients`}>Ingredients</Link>
+          <Link className='hover:bg-slate-800 p-2 hover:cursor-pointer' href={`/admin/tools`}>Tools</Link>
+          <Link className='hover:bg-slate-800 p-2 hover:cursor-pointer' href={`/admin/categories`}>Categories</Link>
         </ul>
     </aside>
   )
