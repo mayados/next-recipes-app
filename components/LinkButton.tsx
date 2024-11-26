dd01c4dfe7a3e6091f5cc2ccb3cad2f6ee87b784
mayados <mariannedos.cours@gmail.com>
2024-11-26 11:39:09 +0100

    add color for text

--- components/LinkButton.tsx
+++ components/LinkButton.tsx
@@ -15,7 +15,7 @@ interface LinkButtonProps {
 
 const LinkButton:React.FC<LinkButtonProps> = ({label, icon: Icon, actionButton, path, dynamicPath}) => {
   return (
-    <Link href={`${path}${dynamicPath}`} className='inline-flex items-center px-3 py-2 mt-3 bg-cyan-900 hover:bg-cyan-800 cursor-pointer rounded-lg' onClick={actionButton}>
+    <Link href={`${path}${dynamicPath}`} className='inline-flex items-center px-3 py-2 mt-3 text-slate-300 bg-cyan-900 hover:bg-cyan-800 cursor-pointer rounded-lg' onClick={actionButton}>
         {/* On rend le texte du bouton variable */}
         {label} 
         <Icon className="mx-2" />
