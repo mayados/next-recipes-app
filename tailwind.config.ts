68bb7f00c6766a933f83c9eaf8974e4719f14578
mayados <mariannedos.cours@gmail.com>
2024-11-26 11:34:19 +0100

    add darkmode to switch and add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,11 +6,17 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'class', // enable dark mode with the 'class' strategy
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        // Custom colors for dark and light mode
+        darkBackground: '#0f172a',
+        darkText: '#e0e0e0',
+        lightBackground: '#ffffff',
+        lightText: '#000000',
       },
     },
   },
