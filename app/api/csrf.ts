579daad55c660c3e76db6fed6246c97730e91cc4
mayados <mariannedos.cours@gmail.com>
2024-11-28 10:02:09 +0100

    comment code for csrf

--- app/api/csrf.ts
+++ app/api/csrf.ts
@@ -1,6 +1,6 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+// import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const csrfToken = generateCsrfToken();
-  res.status(200).json({ csrfToken });
-}
+// export default function handler(req: NextApiRequest, res: NextApiResponse) {
+//   const csrfToken = generateCsrfToken();
+//   res.status(200).json({ csrfToken });
+// }
